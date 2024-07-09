f5f107bc736ff41729d7781e9eb89a0ccc9b761c
imac-1681 <imac-1681@imac-1681s-MacBook-Pro.local>
2024-07-09 16:22:09 +0800

    first commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by imac-1681 on 2024/7/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/fitstPractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitstPractiveApp.swift
+//  Shared
+//
+//  Created by imac-1681 on 2024/7/9.
+//
+
+import SwiftUI
+
+@main
+struct fitstPractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
